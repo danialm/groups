0742783db30b6e36334cf54fd5fe9564bb643f2c
danial <gdanialq@gmail.com>
2014-10-09 18:37:35 -0700

    groups in groups

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require "lessphp/lessc.inc.php";
 
 $less = new lessc;
-$less->checkedCompile("css/main.less", "css/main.css");
+$less->checkedCompile("groups/css/main.less", "css/main.css");
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,7 +12,7 @@ $less->checkedCompile("css/main.less", "css/main.css");
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
         <link rel="icon" href="img/favicon.ico" type="image/x-icon">
-        <link href="css/main.css" rel="stylesheet" />
+        <link href="groups/css/main.css" rel="stylesheet" />
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
